df1f53541da4728767cd07e53e00b989d000cafc
ElizavetaZavyalova <elizabeht@bk.ru>
2023-10-24 15:01:20 +0300

    add

---
+++ src/main/java/org/example/analize/Variables.java
@@ -0,0 +1,14 @@
+package org.example.analize;
+
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.TreeMap;
+@Getter
+public class Variables {
+    Map<String,String> variables=new TreeMap<>();
+    public String addVariableAndMakeItCorrectView(String variable){
+        //TODO parseOppiration String Int Opiration type (not)like is only string
+         return variable;
+    }
+}
