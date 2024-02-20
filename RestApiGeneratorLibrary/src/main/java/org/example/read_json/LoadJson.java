a3b64bc6de6e7d0e9e85e20c46736987f6f786ea
ElizavetaZavyalova <elizabeht@bk.ru>
2024-02-21 00:06:20 +0300

    rainbow logs

--- RestApiGeneratorLibrary/src/main/java/org/example/read_json/LoadJson.java
+++ RestApiGeneratorLibrary/src/main/java/org/example/read_json/LoadJson.java
@@ -3,6 +3,6 @@ package org.example.read_json;
 import java.io.IOException;
 
 
-public interface LoadJson<Result> {
-    public Result  load(String jsonPath) throws IOException;
+public interface LoadJson<R> {
+    public R  load(String jsonPath) throws IOException;
 }
